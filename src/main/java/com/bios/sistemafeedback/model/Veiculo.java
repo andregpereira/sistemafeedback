4cd9633d6abe111a6ac33819223df95227e0971f
Gustavo Santos <69945483+GuuSantos@users.noreply.github.com>
2022-05-20 16:08:49 -0300

    adicionando VeiculoModel no projeto

---
+++ src/main/java/com/bios/sistemafeedback/model/Veiculo.java
@@ -0,0 +1,60 @@
+package com.bios.sistemafeedback.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Veiculo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
+
+	private String placa;
+	private Long serie;
+	private String modelo;
+	private String nomePopular;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public Long getSerie() {
+		return serie;
+	}
+
+	public void setSerie(Long serie) {
+		this.serie = serie;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getNomePopular() {
+		return nomePopular;
+	}
+
+	public void setNomePopular(String nomePopular) {
+		this.nomePopular = nomePopular;
+	}
+
+}
