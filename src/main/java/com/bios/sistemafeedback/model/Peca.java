b0a57bbd299a569206195d9190ac3924fd7f1ee2
Matheus <68198636+mathfp@users.noreply.github.com>
2022-05-20 16:08:09 -0300

    Adição da classe Peca.java
    Adição da classe Peca.java  dentro do pacote model

---
+++ src/main/java/com/bios/sistemafeedback/model/Peca.java
@@ -0,0 +1,42 @@
+package com.bios.sistemafeedback.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Peca {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
+
+	private String nome;
+	private Long serie;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Long getSerie() {
+		return serie;
+	}
+
+	public void setSerie(Long serie) {
+		this.serie = serie;
+	}
+
+}
